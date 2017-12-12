9667c91873496aab79b0049370805a6d5a6f571c
chisun-joung <csjoung@gmail.com>
2017-12-12 21:30:07 +0900

    Log Cat

--- app/src/main/java/com/example/chisunjoung/journaler/Journaler.kt
+++ app/src/main/java/com/example/chisunjoung/journaler/Journaler.kt
@@ -2,6 +2,7 @@ package com.example.chisunjoung.journaler
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 
 /**
  * Created by chisunjoung on 12/12/2017.
@@ -9,11 +10,23 @@ import android.content.Context
 
 class Journaler : Application(){
     companion object {
+        val tag = "Journaler"
         var ctx: Context? = null
     }
 
     override fun onCreate() {
         super.onCreate()
         ctx = applicationContext
+        Log.v(tag,"[ ON CREATE ]")
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        Log.w(tag,"[ ON LOW MEMORY ]")
+    }
+
+    override fun onTrimMemory(level: Int) {
+        super.onTrimMemory(level)
+        Log.d(tag, "[ ON TRIM MEMORY ]: $level")
     }
 }
