ac04366f940b343f92af27050aee4b592f45cb82
chisun-joung <csjoung@gmail.com>
2017-12-18 11:45:29 +0900

    Drawer Navigation ListView  Adapter

---
+++ app/src/main/java/com/example/chisunjoung/journaler/navigation/NavigationDrawerItem.kt
@@ -0,0 +1,9 @@
+package com.example.chisunjoung.journaler.navigation
+
+/**
+ * Created by chisunjoung on 18/12/2017.
+ */
+data class NavigationDrawerItem(
+        val title: String,
+        val onClick: Runnable
+)
