94f801575e4ed20710cf6a3aa366b3353034f0b3
chisun-joung <csjoung@gmail.com>
2017-12-19 11:27:27 +0900

    Connecting activities

--- app/src/main/java/com/example/chisunjoung/journaler/activity/ItemActivity.kt
+++ app/src/main/java/com/example/chisunjoung/journaler/activity/ItemActivity.kt
@@ -1,6 +1,23 @@
 package com.example.chisunjoung.journaler.activity
 
+import android.os.Bundle
+import android.util.Log
+import com.example.chisunjoung.journaler.R
+import com.example.chisunjoung.journaler.model.MODE
+
 /**
  * Created by chisunjoung on 13/12/2017.
  */
-abstract class ItemActivity : BaseActivity()
+abstract class ItemActivity : BaseActivity(){
+    protected var mode = MODE.VIEW
+    override fun getActivityTitle(): Int {
+        return R.string.app_name
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val modeToSet = intent.getIntExtra(MODE.EXTRAS_KEY,MODE.VIEW.mode)
+        mode = MODE.getByValue(modeToSet)
+        Log.v(tag, "Mode[ $mode  ]")
+    }
+}
--- app/src/main/java/com/example/chisunjoung/journaler/fragment/ItemsFragment.kt
+++ app/src/main/java/com/example/chisunjoung/journaler/fragment/ItemsFragment.kt
@@ -1,6 +1,17 @@
 package com.example.chisunjoung.journaler.fragment
 
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.example.chisunjoung.journaler.R
+import com.example.chisunjoung.journaler.activity.NoteActivity
+import com.example.chisunjoung.journaler.activity.TodoActivity
+import com.example.chisunjoung.journaler.model.MODE
 
 /**
  * Created by chisunjoung on 14/12/2017.
@@ -13,5 +24,47 @@ class ItemsFragment : BaseFragement(){
     override fun getLayout(): Int {
 
         return R.layout.fragement_items
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater?.inflate(getLayout(), container, false)
+        val btn = view?.findViewById<FloatingActionButton>(R.id.new_item)
+        btn?.setOnClickListener {
+            val items = arrayOf(
+                    getString(R.string.todos),
+                    getString(R.string.notes)
+            )
+            val builder =
+                    AlertDialog.Builder(this@ItemsFragment.context)
+                            .setTitle(R.string.choose_a_type)
+                            .setItems(
+                                    items,
+                                    {
+                                        _, which ->
+                                        when (which) {
+                                            0 -> {
+                                                openCreateTodo()
+                                            }
+                                            1 -> {
+                                                openCreateNote()
+                                            }
+                                            else -> Log.e(logTag, "Unknown option selected [ $which ]")
+                                        }
+                                    }
+                            )
+            builder.show()
+        }
+        return view
+    }
+
+    private fun openCreateNote(){
+        val intent = Intent(context, NoteActivity::class.java)
+        intent.putExtra(MODE.EXTRAS_KEY, MODE.CREATE.mode)
+        startActivity(intent)
+    }
+    private fun openCreateTodo(){
+        val intent = Intent(context, TodoActivity::class.java)
+        intent.putExtra(MODE.EXTRAS_KEY, MODE.CREATE.mode)
+        startActivity(intent)
     }
 }
