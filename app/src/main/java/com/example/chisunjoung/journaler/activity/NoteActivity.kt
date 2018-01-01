694a9c76c4d7eae3c2db2f27c372f5ea21f10513
chisun-joung <csjoung@gmail.com>
2018-01-01 19:01:52 +0900

    location provider

--- app/src/main/java/com/example/chisunjoung/journaler/activity/NoteActivity.kt
+++ app/src/main/java/com/example/chisunjoung/journaler/activity/NoteActivity.kt
@@ -1,6 +1,18 @@
 package com.example.chisunjoung.journaler.activity
 
+import android.location.Location
+import android.location.LocationListener
+import android.os.AsyncTask
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
+import android.util.Log
 import com.example.chisunjoung.journaler.R
+import com.example.chisunjoung.journaler.database.Db
+import com.example.chisunjoung.journaler.location.LocationProvider
+import com.example.chisunjoung.journaler.model.Note
+import kotlinx.android.synthetic.main.activity_note.*
 
 /**
  * Created by chisunjoung on 13/12/2017.
@@ -15,4 +27,102 @@ class NoteActivity : ItemActivity() {
         get() = "Note Activity"
 
     override fun getActivityTitle() = R.string.app_name
+
+    private var note : Note? = null
+    private var location : Location? = null
+
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(p0: Editable?) {
+            updateNote()
+        }
+
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+    }
+
+    private val locationListener = object : LocationListener {
+        override fun onLocationChanged(p0: Location?) {
+            p0?.let {
+                LocationProvider.unsubscribe(this)
+                location = p0
+                val title = getNoteTitle()
+                val content = getNoteContent()
+                note = Note(title, content, p0)
+                val task = object : AsyncTask<Note, Void, Boolean>() {
+                    override fun doInBackground(vararg p0: Note?): Boolean {
+                        if (!p0.isEmpty()){
+                            val param = p0[0]
+                            param?.let {
+                                return Db.NOTE.insert(param) > 0
+                            }
+                        }
+                        return false
+                    }
+
+                    override fun onPostExecute(result: Boolean?) {
+                        result?.let {
+                            if (result) {
+                                Log.i(tag, "Note inserted.")
+                            } else {
+                                Log.e(tag, "Note not inserted.")
+                            }
+                        }
+                    }
+                }
+                task.execute(note)
+            }
+        }
+        override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {}
+        override fun onProviderEnabled(p0: String?) {}
+        override fun onProviderDisabled(p0: String?) {}
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        note_title.addTextChangedListener(textWatcher)
+        note_content.addTextChangedListener(textWatcher)
+    }
+
+    private fun updateNote() {
+        if (note == null) {
+            if (!TextUtils.isEmpty(getNoteTitle()) &&
+                    !TextUtils.isEmpty(getNoteContent())) {
+                LocationProvider.subscribe(locationListener)
+            }
+        } else {
+            note?.title = getNoteTitle()
+            note?.message = getNoteContent()
+            val task = object : AsyncTask<Note, Void, Boolean>() {
+                override fun doInBackground(vararg params: Note?):
+                        Boolean {
+                    if (!params.isEmpty()) {
+                        val param = params[0]
+                        param?.let {
+                            return Db.NOTE.update(param) > 0
+                        }
+                    }
+                    return false
+                }
+
+                override fun onPostExecute(result: Boolean?) {
+                    result?.let {
+                        if (result) {
+                            Log.i(tag, "Note updated.")
+                        } else {
+                            Log.e(tag, "Note not updated.")
+                        }
+                    }
+                }
+            }
+            task.execute(note)
+        }
+    }
+
+    private fun getNoteContent(): String {
+        return note_content.text.toString()
+    }
+
+    private fun getNoteTitle(): String {
+        return note_title.text.toString()
+    }
 }
--- app/src/main/java/com/example/chisunjoung/journaler/database/Db.kt
+++ app/src/main/java/com/example/chisunjoung/journaler/database/Db.kt
@@ -1,6 +1,7 @@
 package com.example.chisunjoung.journaler.database
 
 import android.content.ContentValues
+import android.location.Location
 import android.util.Log
 import com.example.chisunjoung.journaler.model.Note
 import com.example.chisunjoung.journaler.model.Todo
@@ -51,6 +52,136 @@ object Db {
             return items
         }
 
+        override fun update(what: Note) = update(listOf(what))
+
+        override fun update(what: Collection<Note>): Int {
+            val db = DbHelper(name, version).writableDatabase
+            db.beginTransaction()
+            var updated = 0
+            what.forEach { item ->
+                val values = ContentValues()
+                val table = DbHelper.TABLE_NOTES
+                values.put(DbHelper.COLUMN_TITLE, item.title)
+                values.put(DbHelper.COLUMN_MESSAGE, item.message)
+                values.put(DbHelper.COLUMN_LOCATION_LATITUDE,
+                        item.location.latitude)
+                values.put(DbHelper.COLUMN_LOCATION_LONGITUDE,
+                        item.location.longitude)
+                db.update(table, values, "_id = ?",
+                        arrayOf(item.id.toString()))
+                updated++
+            }
+            val result = updated == what.size
+            if (result) {
+                db.setTransactionSuccessful()
+            } else {
+                updated = 0
+            }
+            db.endTransaction()
+            db.close()
+            return updated
+        }
+
+        override fun delete(what: Note): Int = delete(listOf(what))
+        override fun delete(what: Collection<Note>): Int {
+            val db = DbHelper(name, version).writableDatabase
+            db.beginTransaction()
+            val ids = StringBuilder()
+            what.forEachIndexed { index, item ->
+                ids.append(item.id.toString())
+                if (index < what.size - 1) {
+                    ids.append(", ")
+                }
+            }
+            val table = DbHelper.TABLE_NOTES
+            val statement = db.compileStatement(
+                    "DELETE FROM $table WHERE ${DbHelper.ID} IN ($ids);"
+            )
+            val count = statement.executeUpdateDelete()
+            val success = count > 0
+            if (success) {
+                db.setTransactionSuccessful()
+                Log.i(tag, "Delete [ SUCCESS ][ $count ][ $statement ]")
+            } else {
+                Log.w(tag, "Delete [ FAILED ][ $statement ]")
+            }
+            db.endTransaction()
+            db.close()
+            return count
+        }
+
+        override fun select(
+                args: Pair<String, String>
+        ): List<Note> = select(listOf(args))
+
+        override fun select(args: Collection<Pair<String, String>>):
+                List<Note> {
+            val db = DbHelper(name, version).writableDatabase
+            val selection = StringBuilder()
+            val selectionArgs = mutableListOf<String>()
+            args.forEach { arg ->
+                selection.append("${arg.first} == ?")
+                selectionArgs.add(arg.second)
+            }
+            val result = mutableListOf<Note>()
+            val cursor = db.query(
+                    true,
+                    DbHelper.TABLE_NOTES,
+                    null,
+                    selection.toString(),
+                    selectionArgs.toTypedArray(),
+                    null, null, null, null
+            )
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(cursor.getColumnIndexOrThrow(DbHelper.ID))
+                val titleIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_TITLE)
+                val title = cursor.getString(titleIdx)
+                val messageIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_MESSAGE)
+                val message = cursor.getString(messageIdx)
+                val latitudeIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_LOCATION_LATITUDE)
+                val latitude = cursor.getDouble(latitudeIdx)
+                val longitudeIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_LOCATION_LONGITUDE)
+                val longitude = cursor.getDouble(longitudeIdx)
+                val location = Location("")
+                location.latitude = latitude
+                location.longitude = longitude
+                val note = Note(title, message, location)
+                note.id = id
+                result.add(note)
+            }
+            cursor.close()
+            return result
+        }
+
+        override fun selectAll(): List<Note> {
+            val db = DbHelper(name, version).writableDatabase
+            val result = mutableListOf<Note>()
+            val cursor = db.query(
+                    true,
+                    DbHelper.TABLE_NOTES,
+                    null, null, null, null, null, null, null
+            )
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(cursor.getColumnIndexOrThrow(DbHelper.ID))
+                val titleIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_TITLE)
+                val title = cursor.getString(titleIdx)
+                val messageIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_MESSAGE)
+                val message = cursor.getString(messageIdx)
+                val latitudeIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_LOCATION_LATITUDE)
+                val latitude = cursor.getDouble(latitudeIdx)
+                val longitudeIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_LOCATION_LONGITUDE)
+                val longitude = cursor.getDouble(longitudeIdx)
+                val location = Location("")
+                location.latitude = latitude
+                location.longitude = longitude
+                val note = Note(title, message, location)
+                note.id = id
+                result.add(note)
+            }
+            cursor.close()
+            return result
+        }
+
     }
 
     val TODO = object : Crud<Todo>{
@@ -91,6 +222,140 @@ object Db {
             db.endTransaction()
             db.close()
             return items
+        }
+
+        override fun update(what: Todo) = update(listOf(what))
+
+        override fun update(what: Collection<Todo>): Int {
+            val db = DbHelper(name, version).writableDatabase
+            db.beginTransaction()
+            var updated = 0
+            what.forEach { item ->
+                val table = DbHelper.TABLE_TODOS
+                val values = ContentValues()
+                values.put(DbHelper.COLUMN_TITLE, item.title)
+                values.put(DbHelper.COLUMN_MESSAGE, item.message)
+                values.put(DbHelper.COLUMN_LOCATION_LATITUDE,
+                        item.location.latitude)
+                values.put(DbHelper.COLUMN_LOCATION_LONGITUDE,
+                        item.location.longitude)
+                values.put(DbHelper.COLUMN_SCHEDULED, item.scheduledFor)
+                db.update(table, values, "_id = ?",
+                        arrayOf(item.id.toString()))
+                updated++
+            }
+            val result = updated == what.size
+            if (result) {
+                db.setTransactionSuccessful()
+            } else {
+                updated = 0
+            }
+            db.endTransaction()
+            db.close()
+            return updated
+        }
+
+        override fun delete(what: Todo): Int = delete(listOf(what))
+        override fun delete(what: Collection<Todo>): Int {
+            val db = DbHelper(name, version).writableDatabase
+            db.beginTransaction()
+            val ids = StringBuilder()
+            what.forEachIndexed { index, item ->
+                ids.append(item.id.toString())
+                if (index < what.size - 1) {
+                    ids.append(", ")
+                }
+            }
+            val table = DbHelper.TABLE_TODOS
+            val statement = db.compileStatement(
+                    "DELETE FROM $table WHERE ${DbHelper.ID} IN ($ids);"
+            )
+            val count = statement.executeUpdateDelete()
+            val success = count > 0
+            if (success) {
+                db.setTransactionSuccessful()
+                Log.i(tag, "Delete [ SUCCESS ][ $count ][ $statement ]")
+            } else {
+                Log.w(tag, "Delete [ FAILED ][ $statement ]")
+            }
+            db.endTransaction()
+            db.close()
+            return count
+        }
+
+        override fun select(args: Pair<String, String>): List<Todo> =
+                select(listOf(args))
+
+        override fun select(args: Collection<Pair<String, String>>):
+                List<Todo> {
+            val db = DbHelper(name, version).writableDatabase
+            val selection = StringBuilder()
+            val selectionArgs = mutableListOf<String>()
+            args.forEach { arg ->
+                selection.append("${arg.first} == ?")
+                selectionArgs.add(arg.second)
+            }
+            val result = mutableListOf<Todo>()
+            val cursor = db.query(
+                    true,
+                    DbHelper.TABLE_NOTES,
+                    null,
+                    selection.toString(),
+                    selectionArgs.toTypedArray(),
+                    null, null, null, null
+            )
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(cursor.getColumnIndexOrThrow(DbHelper.ID))
+                val titleIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_TITLE)
+                val title = cursor.getString(titleIdx)
+                val messageIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_MESSAGE)
+                val message = cursor.getString(messageIdx)
+                val latitudeIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_LOCATION_LATITUDE)
+                val latitude = cursor.getDouble(latitudeIdx)
+                val longitudeIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_LOCATION_LONGITUDE)
+                val longitude = cursor.getDouble(longitudeIdx)
+                val location = Location("")
+                val scheduledForIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_SCHEDULED)
+                val scheduledFor = cursor.getLong(scheduledForIdx)
+                location.latitude = latitude
+                location.longitude = longitude
+                val todo = Todo(title, message, location, scheduledFor)
+                todo.id = id
+                result.add(todo)
+            }
+            cursor.close()
+            return result
+        }
+
+        override fun selectAll(): List<Todo> {
+            val db = DbHelper(name, version).writableDatabase
+            val result = mutableListOf<Todo>()
+            val cursor = db.query(
+                    true,
+                    DbHelper.TABLE_NOTES,
+                    null, null, null, null, null, null, null
+            )
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(cursor.getColumnIndexOrThrow(DbHelper.ID))
+                val titleIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_TITLE)
+                val title = cursor.getString(titleIdx)
+                val messageIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_MESSAGE)
+                val message = cursor.getString(messageIdx)
+                val latitudeIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_LOCATION_LATITUDE)
+                val latitude = cursor.getDouble(latitudeIdx)
+                val longitudeIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_LOCATION_LONGITUDE)
+                val longitude = cursor.getDouble(longitudeIdx)
+                val location = Location("")
+                val scheduledForIdx = cursor.getColumnIndexOrThrow(DbHelper.COLUMN_SCHEDULED)
+                val scheduledFor = cursor.getLong(scheduledForIdx)
+                location.latitude = latitude
+                location.longitude = longitude
+                val todo = Todo(title, message, location, scheduledFor)
+                todo.id = id
+                result.add(todo)
+            }
+            cursor.close()
+            return result
         }
     }
 }
